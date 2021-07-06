a25589f34ee871ccc7b264d4f0deb702297fcb65
kscheltema <k87scheltema@outlook.com>
2021-07-06 17:12:10 +0200

    protecting authorised pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import { Switch, Route } from 'react-router-dom';
-
+import { useContext } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import AuthContext from './store/auth-context';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
 
 function App() {
+  const authCTX = useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
         <Route path='/' exact component={HomePage} />
-        <Route path='/auth' component={AuthPage} />
-        <Route path='/profile' component={UserProfile} />
+        {!authCTX.isLoggedIn && <Route path='/auth' component={AuthPage} />}
+       {authCTX.isLoggedIn && <Route path='/profile' component={UserProfile} />}
+       <Route path='*'>
+      <Redirect to='/'/>
+       </Route>
       </Switch>
     </Layout>
   );
